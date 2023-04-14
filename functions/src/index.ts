305fc0779196b6fa1e762672bb65f903c4cc93eb
jin <kidkid70@naver.com>
2023-04-14 12:09:09 +0900

    Implement Push Notification using Firebase Messaging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,62 +6,82 @@ admin.initializeApp();
 
 
 export const onVideoCreated = functions.firestore
-    .document("videos/{videoId}")
-    .onCreate(async (snapshot, context) => {
-        const spawn = require("child-process-promise").spawn;
-        const video = snapshot.data();
-        await spawn("ffmpeg", [
-            "-i", video.fileUrl,
-            "-ss", "00:00:01.000",
-            "-vframes", "1",
-            "-vf", "scale=150:-1",
-            `/tmp/${snapshot.id}.jpg`,
-        ]);
+  .document("videos/{videoId}")
+  .onCreate(async (snapshot, context) => {
+    const spawn = require("child-process-promise").spawn;
+    const video = snapshot.data();
+    await spawn("ffmpeg", [
+      "-i", video.fileUrl,
+      "-ss", "00:00:01.000",
+      "-vframes", "1",
+      "-vf", "scale=150:-1",
+      `/tmp/${snapshot.id}.jpg`,
+    ]);
 
-        const storage = admin.storage();
-        const [file, _] = await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
-            destination: `thumbnails/${snapshot.id}.jpg`,
-        });
-
-        await file.makePublic();
-        await snapshot.ref.update({ thumbnailUrl: file.publicUrl() });
-        const db = admin.firestore();
-
-        await db
-            .collection("videos")
-            .doc(snapshot.id)
-            .update({
-                videoId: snapshot.id,
-            });
-
-        await db
-            .collection("users")
-            .doc(video.creatorUid)
-            .collection("videos")
-            .doc(snapshot.id)
-            .set({
-                thumbnailUrl: file.publicUrl(),
-                videoId: snapshot.id,
-            });
-
+    const storage = admin.storage();
+    const [file, _] = await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+      destination: `thumbnails/${snapshot.id}.jpg`,
     });
+
+    await file.makePublic();
+    await snapshot.ref.update({ thumbnailUrl: file.publicUrl() });
+    const db = admin.firestore();
+
+    await db
+      .collection("videos")
+      .doc(snapshot.id)
+      .update({
+        videoId: snapshot.id,
+      });
+
+    await db
+      .collection("users")
+      .doc(video.creatorUid)
+      .collection("videos")
+      .doc(snapshot.id)
+      .set({
+        thumbnailUrl: file.publicUrl(),
+        videoId: snapshot.id,
+      });
+
+  });
 
 export const onLikedCreated = functions.firestore
-    .document("likes/{likeId}")
-    .onCreate(async (snapshot, context) => {
-        const db = admin.firestore();
-        const [videoId, _] = snapshot.id.split("000");
-        await db.collection("videos").doc(videoId).update({
-            likes: admin.firestore.FieldValue.increment(1)
-        });
+  .document("likes/{likeId}")
+  .onCreate(async (snapshot, context) => {
+    const db = admin.firestore();
+
+    const [videoId, _] = snapshot.id.split("000");
+
+    await db.collection("videos").doc(videoId).update({
+      likes: admin.firestore.FieldValue.increment(1)
     });
 
-export const onLikedRemoved = functions.firestore
-    .document("likes/{likeId}")
-    .onDelete(async (snapshot, context) => {
-        const db = admin.firestore();
-        const [videoId, _] = snapshot.id.split("000");
-        await db.collection("videos").doc(videoId).update({
-            likes: admin.firestore.FieldValue.increment(-1)
+    const video = (await db.collection("videos").doc(videoId).get()).data();
+    if (video) {
+      const creatorUid = video.creatorUid;
+      const user = (await db.collection("users").doc(creatorUid).get()).data();
+      if (user) {
+        const token = user.token;
+        await admin.messaging().sendToDevice(token, {
+          data: {
+            screen: "123",
+          },
+          notification: {
+            title: "Someone liked your video!",
+            body: "Likes + 1 ! Congrats! ♥",
+          },
         });
+      }
+    }
+  });
+
+export const onLikedRemoved = functions.firestore
+  .document("likes/{likeId}")
+  .onDelete(async (snapshot, context) => {
+    const db = admin.firestore();
+    const [videoId, _] = snapshot.id.split("000");
+    await db.collection("videos").doc(videoId).update({
+      likes: admin.firestore.FieldValue.increment(-1)
     });
+  });
